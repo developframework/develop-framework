509e213057d54548f7912882531cd8c140b98f89
qiushui <408000511@qq.com>
2018-07-09 15:07:02 +0800

    添加新组件

---
+++ develop-framework-commons/src/main/java/develop/framework/commons/components/SimplePager.java
@@ -0,0 +1,16 @@
+package develop.framework.commons.components;
+
+/**
+ * @author qiushui on 2018-07-02.
+ * @since 0.4
+ */
+public class SimplePager extends Pager {
+
+	public SimplePager() {
+
+	}
+
+	public SimplePager(int index, int size) {
+		super(index, size);
+	}
+}
